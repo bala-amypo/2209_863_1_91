03878d1858991e1b8ffadbb51a91e7b2894b7f65
bala-amypo <bala@amypo.in>
2025-12-18 16:48:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/CategorizationLog.java
+++ src/main/java/com/example/demo/entity/CategorizationLog.java
@@ -13,5 +13,17 @@ public class CategorizationLog{
 
     private Integet appliedRule;
 
+    private String matchedKeyword;
+
+    private String assignedCategory;
+
+    private String assignedUrgency;
+
+    private LocalDateTime loggedAt;
+
+    @PrePerist
+    public void prePersist(){
+        loggedAt=LocalDateTime.now();
+    }
     
 }
