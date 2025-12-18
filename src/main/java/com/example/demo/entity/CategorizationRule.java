24e567ff0e2d5f58cfc92ed6979369bc1768d801
bala-amypo <bala@amypo.in>
2025-12-18 16:38:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/CategorizationRule.java
+++ src/main/java/com/example/demo/entity/CategorizationRule.java
@@ -1,1 +1,20 @@
-packGE
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CategorizationRule(){
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
+
+    @NotBlank
+    private String keyword;
+
+    private String matchType;
+
+    private Integer priorri
+}
