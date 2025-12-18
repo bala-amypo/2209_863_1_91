8c06b86f4867672ddfdf0ca3369e45d9b32a352c
bala-amypo <bala@amypo.in>
2025-12-18 16:43:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/UrgencyPolicy.java
+++ src/main/java/com/example/demo/entity/UrgencyPolicy.java
@@ -7,5 +7,21 @@ public class UrgencyPolicy{
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    pr
+    private Long id;
+
+    private String policyName;
+
+    @Column(nullable=false)
+    private String keyword;
+
+    @Column(nullable=false)
+    private String urgencyOverride;
+
+    private LocalDateTime createdAt;
+
+`   @PrePersist
+    public void prePersist(){
+        createdAt=LocalDateTime.now();
+    }
+    
 }
