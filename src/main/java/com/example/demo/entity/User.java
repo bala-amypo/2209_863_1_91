b92f23fe5607075ca5c615e32d6479628f8ac880
bala-amypo <bala@amypo.in>
2025-12-18 16:17:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -35,10 +35,30 @@ public class User{
     this.id=id;
     }
 
-    public String getfullName(){
+    public String getFullName(){
     return fullName;
     }
 
-    public void setfullName()
+    public void setFullName(String fullName){
+    this.fullName=fullName;
+    }
+
+    public String getEmail(){
+    return email;
+    }
+
+    public void setEmail(String email){
+    this.email=email;
+    }
+
+    public String getPassword(){
+    return password;
+    }
+
+    public void setPassword(String password){
+    this.password=password;
+    }
+
+    
 
 }
