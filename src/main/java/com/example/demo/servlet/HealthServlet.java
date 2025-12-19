e06f6b00e9427e5cd2a22504dd9711234dde596b
bala-amypo <bala@amypo.in>
2025-12-19 04:10:11 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HealthServlet.java
+++ src/main/java/com/example/demo/servlet/HealthServlet.java
@@ -3,3 +3,10 @@ package com.example.demo.servlet;
 import jakarta.persistence.*;
 
 @WebServlet(urlPatterns="/health")
+public class HealthServlet extends HttpServlet{
+
+    protected void deoGet(HttpServletRequest req,HttpServlet resp) throws IOException
+    {
+        resp.getWriter().write("OK");
+    }
+}
