100e38071b253d54a152ea5bdbfa1a2307aa8d47
bala-amypo <bala@amypo.in>
2025-12-19 04:33:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,6 +1,7 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="categories")
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -71,10 +71,6 @@ public class User{
     public LocalDateTime getCreatedAt(){
     return createdAt;
     }
-
-    public void setCreatedAt(LocalDateTime createdAt){
-    this.createdAt=createdAt;
-    }
     
     public User(){
 
