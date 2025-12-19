e34d3bf49f0a41ba80e43e1989b23bbaff660542
bala-amypo <bala@amypo.in>
2025-12-19 08:40:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationRuleController.java
+++ src/main/java/com/example/demo/controller/CategorizationRuleController.java
@@ -6,3 +6,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/api/rules")
+public class CategorizationRuleController{
+
+    private final CategorizationRuleService categorizationRuleService;
+    public CategorizationRuleController(CategorizationRuleService categorizationRuleService){
+        this.categorizationRuleService=categorizationRuleService;
+    }
+
+    @PostMapping("/{categoryId}")
+}
