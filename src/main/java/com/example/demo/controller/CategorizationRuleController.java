01f7020705ad8e92e56876ca42729f834fe1615a
bala-amypo <bala@amypo.in>
2025-12-19 08:37:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationRuleController.java
+++ src/main/java/com/example/demo/controller/CategorizationRuleController.java
@@ -1,1 +1,8 @@
-package 
+package com.example.demo.controller;
+
+import com.example.demo.model.CategorizationRule;
+import com.example.demo.service.CategorizationRuleService;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
