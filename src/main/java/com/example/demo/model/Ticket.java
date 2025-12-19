5263ae989ecbdbb6a9598a6208db4e1856f1636a
bala-amypo <bala@amypo.in>
2025-12-19 05:00:12 +0000

    User commit

--- src/main/java/com/example/demo/model/Ticket.java
+++ src/main/java/com/example/demo/model/Ticket.java
@@ -32,4 +32,22 @@ public class Ticket{
 
     private String urgencyLevel;
 
+    public Long getId(){
+    return id;
+    }
+
+    public void setId(Long id){
+    this.id=id;
+    }
+
+    public String getTitle(){
+    return title;
+    }
+
+    public void setTitle(String title){
+    this.title=title;
+    }
+
+    public 
+
 }
