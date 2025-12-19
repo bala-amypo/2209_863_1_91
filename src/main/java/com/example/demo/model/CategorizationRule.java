e45f3a6708330240111af013644e292df2463951
bala-amypo <bala@amypo.in>
2025-12-19 05:25:48 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -27,7 +27,27 @@ public class CategorizationRule(){
     }
 
     public Long getId(){
-        
+        return id;
+    }
+
+    public Long setId(Long id){
+        this.id=id;
+    }
+
+    public Category getCategory(){
+        return category;
+    }
+
+    public void setCategory(Category category){
+        this.category=category;
+    }
+
+    public String getKeyword(){
+        return keyword;
+    }
+
+    public void setKeyword(String keyword){
+        this.keyword=keyword;
     }
 
     
