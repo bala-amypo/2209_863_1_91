abf3df86ecbc8fb4954a97988dd1908fe6b09bf0
bala-amypo <bala@amypo.in>
2025-12-19 08:07:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -1,1 +1,8 @@
-package 
+package com.example.demo.controller;
+
+import com.example.demo.model.Ticket;
+import com.example.demo.service.TicketService;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
