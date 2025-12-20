1670086c0c7f10fb20a3e66687eeb1b0a3cd3f10
bala-amypo <bala@amypo.in>
2025-12-20 05:58:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationEngineController.java
+++ src/main/java/com/example/demo/controller/CategorizationEngineController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.CategorizationLog;
-import com.example.demo.service.
+import com.example.demo.service.CategorizationLogService;
+import org.springframework.we.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/categorize")
+public class CategorizationEngineController{
+
+    private final CategorizationLogService CategorizationLogService;
+    public 
+}
