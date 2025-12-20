243bffb26e261937efcb0835ef70cadca388509e
bala-amypo <bala@amypo.in>
2025-12-20 08:49:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
@@ -5,3 +5,16 @@ import com.example.demo.model.CategorizationRule;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.CategorizationRuleRepository;
 import com.example.demo.service.CategorizationRuleService;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class CategorizationRuleServiceImpl implements CategorizationRuleService{
+
+    private final CategorizationRuleRepository categorizationRuleRepository;
+    private 
+    public CategorizationRuleServiceImpl(CategorizationRuleRepository categorizationRuleRepository){
+        this.categorizationRuleRepository=categorizationRuleRepository;
+    }
+    
+}
