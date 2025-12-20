858510d62c262a93aad58936649d94594c6830c3
bala-amypo <bala@amypo.in>
2025-12-20 05:49:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/UrgencyPolicyController.java
+++ src/main/java/com/example/demo/controller/UrgencyPolicyController.java
@@ -1,3 +1,16 @@
 package dom.example.model.controller;
 
 import dom.example.model.UrgencyPolicy;
+import dom.example.service.UrgencyPolicyService;
+import org.springframework.web.bind.annotation.;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/policies")
+public class UrgencyPolicyController{
+
+    private final UrgencyPolicyService urgencyPolicyService;
+    public UrgencyPolicyController(UrgencyPolicyService urgencyPolicyService){
+        this.urgencyPolicyService=urgencyPolicyService;
+    }
+}
