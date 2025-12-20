90e14fa1ea05b275bf6ae7dbe161fd38aa1611eb
bala-amypo <bala@amypo.in>
2025-12-20 09:01:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
@@ -1,3 +1,16 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.
+import com.example.demo.model.UrgencyPolicy;
+import com.example.demo.repository.UrgencyPolicyRepository;
+import com.example.demo.service.UrgencyPolicyService;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class UrgencyPolicyServiceImpl implements UrgencyPolicyService{
+
+    private final UrgencyPolicyRepository policyRepository;
+    public UrgencyPolicyServiceImpl(UrgencyPolicyRepository policyRepository){
+        this.policyRepository=policyRepository;
+    }
+}
