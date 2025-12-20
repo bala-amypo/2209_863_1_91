dc0e858e73a1171c53b190f70f0606ebbe62ad88
bala-amypo <bala@amypo.in>
2025-12-20 09:58:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/CategorizationLogRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.CategorizationLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+public interface Categor
