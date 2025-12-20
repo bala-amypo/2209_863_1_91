f1476e03496d1abebbec416496ecb7bdb56c6b54
bala-amypo <bala@amypo.in>
2025-12-20 08:31:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
@@ -2,3 +2,13 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.Ticket;
 import com.example.demo.repository.TicketRepository;
+import com.example.demo.service.TicketService;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class TicketServiceImpl implements TicketService{
+    private final TicketRepository ticketRepository;
+    public TicketServiceImpl(TicketRepository ticketRepository){
+        this.ticketRepository
+    }
