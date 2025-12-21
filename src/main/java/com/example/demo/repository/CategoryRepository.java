baa7f84e66f7d42b79ed73205e8547d9589fb984
bala-amypo <bala@amypo.in>
2025-12-21 13:23:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long>{
-        boolean existsByCategoryName(String categoryName);
+        //boolean existsByCategoryName(String categoryName);
 }
