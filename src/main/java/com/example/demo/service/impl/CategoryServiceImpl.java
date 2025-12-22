3cf8db21dfeda0b8faf50ddc07bf9dd6c997b08d
bala-amypo <bala@amypo.in>
2025-12-22 13:52:46 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,32 +1,37 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategoryService;
 import org.springframework.stereotype.Service;
-import com.example.demo.exception.ResourceNotFoundException;
-import java.util.*; 
+import java.util.List;
 
 @Service
-public class CategoryServiceImpl implements CategoryService{
-
+public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
-    public CategoryServiceImpl(CategoryRepository categoryRepository){
-        this.categoryRepository=categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public Category createCategory(Category category){
+    public Category createCategory(Category category) {
+        if (category == null) {
+            throw new BadRequestException("Category not found");
+        }
         return categoryRepository.save(category);
     }
 
     @Override
-    public List<Category> getAllCategories(){
-        return categoryRepository.findAll();
+    public Category getCategory(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
     }
 
     @Override
-    public Category getCategory(Long id){
-        return categoryRepository.findById(id).orElseThrow(()->new RuntimeException("Category not found"));
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
     }
 }
