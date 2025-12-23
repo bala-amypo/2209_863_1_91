6145d727852912e7758632154b0b0f5a5a1a7f9f
bala-amypo <bala@amypo.in>
2025-12-23 03:32:23 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-/*import org.springframework.security.core.userdetails.UsernameNotFoundException;
+/*import org.springframework.security.core.userdetails.UsernameNotFoundException;*/
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
