30f400262ea36ef4ffdcf9e460dc2f23a7956271
bala-amypo <bala@amypo.in>
2025-12-23 03:24:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.*;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.UrgencyPolicy;
 import com.example.demo.repository.UrgencyPolicyRepository;
@@ -19,7 +19,7 @@ public class UrgencyPolicyServiceImpl implements UrgencyPolicyService {
     @Override
     public UrgencyPolicy createPolicy(UrgencyPolicy policy) {
         if (policy == null) {
-            throw new BadRequestException("Policy not found");
+            throw new RuntimeException("Policy not found");
         }
         return policyRepository.save(policy);
     }
